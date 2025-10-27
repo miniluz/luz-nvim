2b686de0357184a20041a39c78bb635327b72c97
miniluz <javiermelon4fu@gmail.com>
2025-10-27 13:39:59 +0100

    typst fixes

--- nvim/after/lsp/typst/typst.lua
+++ nvim/after/lsp/typst/typst.lua
@@ -1,4 +1,2 @@
-o.textwidth = 120
+vim.o.textwidth = 120
 vim.opt_local.formatoptions:append({ t = true }) -- Wrap all text, not just comments
-
-vim.keymap.set("n", "<leader>p", vim.cmd.TypstPreview, { buffer = true })
