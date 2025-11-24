fa6634dcefcb7ad7fabf00d3ea73a8156c6925c3
miniluz <javiermelon4fu@gmail.com>
2025-11-24 08:41:58 +0100

    Fix spelling on markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 vim.wo.wrap = true
 vim.bo.textwidth = 120
 vim.opt_local.formatoptions:append({ t = true }) -- Wrap all text, not just comments
-vim.bo.spelllang = { "en", "es" }
+vim.bo.spelllang = "en,es"
 vim.bo.spell = true
 
 vim.keymap.set("n", "<leader>p", vim.cmd.MarkdownPreview, { buffer = true })
