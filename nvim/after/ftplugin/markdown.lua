02eaf63ee968c393470b07878dd01eaad66258a5
miniluz <javiermelon4fu@gmail.com>
2025-11-24 08:44:51 +0100

    Oops, guess I didn't fix it

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -2,6 +2,6 @@ vim.wo.wrap = true
 vim.bo.textwidth = 120
 vim.opt_local.formatoptions:append({ t = true }) -- Wrap all text, not just comments
 vim.bo.spelllang = "en,es"
-vim.bo.spell = true
+vim.wo.spell = true
 
 vim.keymap.set("n", "<leader>p", vim.cmd.MarkdownPreview, { buffer = true })
